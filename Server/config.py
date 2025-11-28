4aec239dc7922dd15bfb4eeba9c9fd2153271f1d
Mathieu7483 <11436@holbertonstudents.com>
2025-11-28 10:53:38 +0100

    Add configuration settings for database and JWT in config.py

--- Server/config.py
+++ Server/config.py
@@ -1,3 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Config:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///pharma_dashboard.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_SECRET_KEY = 'your_jwt_secret_key'
+    JWT_ACCESS_TOKEN_EXPIRES = 3600
+
+class DevelopmentConfig(Config):
+      DEBUG = True
