928042b86891326e1cdf85c0df913ad672736881
Mathieu7483 <11436@holbertonstudents.com>
2025-11-28 10:54:55 +0100

    Add email and is_admin fields to UserModel

--- Server/models/user.py
+++ Server/models/user.py
@@ -7,6 +7,9 @@ class UserModel(BaseModel):
   username = db.Column(db.String(80), unique=True, nullable=False)
   password_hash = db.Column(db.String(128), nullable=False)
 
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  is_admin = db.Column(db.Boolean, default=False)
+
   def set_password(self, password):
       self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
   def check_password(self, password):
