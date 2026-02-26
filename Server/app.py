7ffa5e53189c1b536202c90ed80473bd0af2d9e6
Mathieu7483 <mathieu.godalier@orange.fr>
2026-02-26 15:27:53 +0100

    Add notes namespace to API for improved note management functionality

--- Server/app.py
+++ Server/app.py
@@ -55,6 +55,7 @@ def create_app(config_class=DevelopmentConfig):
     from api.analytics    import analytics_ns
     from api.chatbot      import ns as chatbot_ns
     from api.inventory    import inventory_ns
+    from api.notes        import notes_ns
     from api.tickets      import tickets_ns
     from api.calendar_events import calendar_ns
 
@@ -67,6 +68,7 @@ def create_app(config_class=DevelopmentConfig):
     api.add_namespace(analytics_ns, path='/analytics')
     api.add_namespace(chatbot_ns,   path='/chatbot')
     api.add_namespace(inventory_ns, path='/inventory')
+    api.add_namespace(notes_ns,     path='/notes')
     api.add_namespace(tickets_ns,   path='/tickets')
     api.add_namespace(calendar_ns,  path='/calendar')
 
