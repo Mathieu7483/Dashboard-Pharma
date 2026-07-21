39d4e8e378e04a0cd63123b8072a63c61edd32c1
Mathieu7483 <mathieu.godalier@orange.fr>
2026-07-21 19:56:08 +0200

    Remove console log statement from login submit handler

--- Client/javascript/auth.js
+++ Client/javascript/auth.js
@@ -81,7 +81,6 @@ document.addEventListener('DOMContentLoaded', () => {
             });
 
             const data = await response.json();
-            console.log(data);
 
             if (response.ok) {
                 CookieManager.set('access_token', data.access_token, 1);
