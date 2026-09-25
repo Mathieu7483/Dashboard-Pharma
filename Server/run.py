2274d7f108a036d04556bf14fd4e027a5fc2c4ff
Mathieu7483 <mathieu.godalier@orange.fr>
2026-09-25 15:56:52 +0200

    feat: Refactor database initialization and seeding process to ensure proper context management

--- Server/run.py
+++ Server/run.py
@@ -4,20 +4,23 @@ from utils.seeder import seed_all_initial_data
 
 app = create_app()
 
-with app.app_context():
-    print("\n--- 🔧 Database Initialization ---")
-    db.create_all()
-
-    print("--- 🌱 Seeding Process ---")
-    try:
-        seed_all_initial_data()
-        print("✅ Seeding completed successfully.")
-    except Exception as e:
-        print(f"❌ Seeding failed: {str(e)}")
-
-print("\n--- 🚀 Starting Pharma Server ---")
-print("Documentation: http://127.0.0.1:5000/docs")
-
 if __name__ == "__main__":
-    # use_reloader=False évite de relancer le Seeding 2 fois au démarrage
+    with app.app_context():
+        print("\n--- 🔧 Database Initialization ---")
+        db.create_all()
+
+        print("--- 🌱 Seeding Process ---")
+        try:
+            seed_all_initial_data()
+            print("✅ Seeding completed successfully.")
+        except Exception as e:
+            print(f"❌ Seeding failed: {str(e)}")
+
+    print("\n--- 🚀 Starting Pharma Server ---")
+    print("Documentation: http://127.0.0.1:5000/docs")
+
+    print("\n--- 🗺️ Routes enregistrées ---")
+    for rule in app.url_map.iter_rules():
+        print(f"{rule} -> {rule.endpoint}")
+    
     app.run(host="0.0.0.0", debug=True, port=5000, use_reloader=False)
